372e24fa4c885e5e4e8a165ca089f7eec88b84db
Nasher3296 <ignaciofernandez3296@gmail.com>
2025-09-30 22:09:42 -0300

    fix

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -6,14 +6,14 @@ import (
 	"time"
 )
 
-func getEnv(key, fallback string) string {
+func GetEnv(key, fallback string) string {
 	if value := os.Getenv(key); value != "" {
 		return value
 	}
 	return fallback
 }
 
-func getEnvInt(key string, fallback int) int {
+func GetEnvInt(key string, fallback int) int {
 	if value := os.Getenv(key); value != "" {
 		if intValue, err := strconv.Atoi(value); err == nil {
 			return intValue
@@ -22,7 +22,7 @@ func getEnvInt(key string, fallback int) int {
 	return fallback
 }
 
-func getEnvDuration(key string, fallback time.Duration) time.Duration {
+func GetEnvDuration(key string, fallback time.Duration) time.Duration {
 	if value := os.Getenv(key); value != "" {
 		if duration, err := time.ParseDuration(value); err == nil {
 			return duration
