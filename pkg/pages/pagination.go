7e2d019fef4b96f0dd2443ff887ffcc56f662746
Nasher3296 <ignaciofernandez3296@gmail.com>
2025-10-05 15:10:57 -0300

    pagination utils

--- pkg/pages/pagination.go
+++ pkg/pages/pagination.go
@@ -1,6 +1,11 @@
 package pages
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+)
 
 type PaginationResult struct {
 	Page         uint `json:"page"`
@@ -16,4 +21,27 @@ type Pagination struct {
 
 func (p *Pagination) GetURLQuery() string {
 	return fmt.Sprintf("page=%d&pagesize=%d", p.Page, p.PageSize)
+}
+
+func FromURLQuery(r *http.Request, defaultPageSize uint64, defaultPage uint64) Pagination {
+	page, _ := strconv.ParseUint(r.URL.Query().Get("page"), 10, 64)
+	if page == 0 {
+		page = defaultPage
+	}
+	pageSize, _ := strconv.ParseUint(r.URL.Query().Get("page_size"), 10, 64)
+	if pageSize == 0 {
+		pageSize = defaultPageSize
+	}
+
+	return Pagination{
+		Page:     uint(page),
+		PageSize: uint(pageSize),
+	}
+}
+
+func (p *Pagination) Validate(maxPageSize uint) error {
+	if p.PageSize > maxPageSize {
+		return errors.New("page size must be less than or equal to max page size")
+	}
+	return nil
 }
