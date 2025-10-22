e0dc03a0204280abd250711e579dcbd70f56a5c8
Nasher3296 <ignaciofernandez3296@gmail.com>
2025-10-21 22:18:20 -0300

    regiones

--- pkg/region/region.go
+++ pkg/region/region.go
@@ -4,12 +4,45 @@ type Region string
 
 const (
 	Global  Region = "global"
+	Africa  Region = "africa"
 	America Region = "america"
-	Europe  Region = "europe"
 	Asia    Region = "asia"
+	Europe  Region = "europe"
 	Oceania Region = "oceania"
 )
 
-func (r Region) String() string {
-	return string(r)
+const (
+	bitAfrica  = 1 << iota // 00001
+	bitAmerica             // 00010
+	bitAsia                // 00100
+	bitEurope              // 01000
+	bitOceania             // 10000
+)
+
+var regionBitMap = map[Region]int{
+	Africa:  bitAfrica,
+	America: bitAmerica,
+	Asia:    bitAsia,
+	Europe:  bitEurope,
+	Oceania: bitOceania,
+}
+
+func FromInt(mask int) []Region {
+	var regions []Region
+	for region, bit := range regionBitMap {
+		if mask&bit != 0 {
+			regions = append(regions, region)
+		}
+	}
+	return regions
+}
+
+func ToInt(regions []Region) int {
+	var mask int
+	for _, region := range regions {
+		if bit, ok := regionBitMap[region]; ok {
+			mask |= bit
+		}
+	}
+	return mask
 }
